d769a2bd9dddbbdba477fb1fb0d610eb02ae7c6e
09eragera09 <gg873702@gmail.com>
2017-12-10 04:52:59 +0530

    Added source and invite links

---
+++ commands/source_and_invite.js
@@ -0,0 +1,6 @@
+"use strict";
+
+module.exports.make = (bot) => {
+    bot.registerCommand("source", "https://github.com/09eragera09/00-Unit", {description: "Gets the bot's source code"});
+    bot.registerCommand("invite", `https://discordapp.com/oauth2/authorize/?permissions=8&scope=bot&client_id=376917136980836362`, {description: "Gets the bot invite url"})
+} 
