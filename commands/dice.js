b58788ed47f132ccd965dfc3ec850d3a332bd6a8
Gaurav Gupta <gg873702@gmail.com>
2017-12-31 05:50:28 +0530

    Now has a dice

---
+++ commands/dice.js
@@ -0,0 +1,18 @@
+"use strict";
+const d20 = require('d20');
+
+module.exports.make = (bot) => {
+    bot.registerCommand("roll", (message, args) => {
+        let array1 = args.toString().split("#");
+        let roll = d20.roll(array1[0], true);
+        let rollString = roll.join("+");
+        let rollTotal = roll.reduce((a, b) => {return a+b});
+        let dice = "`"+array1[0]+"`";
+        bot.createMessage(message.channel.id, {
+            content: `${message.author.mention}`+': '+`${dice}`+' = ('+`${rollString}`+') = ' + `${rollTotal}`
+        })
+    }, {
+        description: "RNGesus is impartial",
+        fullDescription: "Rolls x number of y sided dice, do !roll xdy+(any other buffs)"
+    })
+}
