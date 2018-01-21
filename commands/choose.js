2956432270b92adc28e0e52c5ef2181d132166a7
Gaurav Gupta <gg873702@gmail.com>
2018-01-22 03:51:16 +0530

    fixed a typo

--- commands/choose.js
+++ commands/choose.js
@@ -4,7 +4,7 @@ const random = require('lodash.random')
 
 module.exports.make = (bot) => {
     bot.registerCommand("choose", (message, args) => {
-        var choiceResult = args.join("").split("|");
+        var choiceResult = args.join(" ").split("|");
         bot.createMessage(message.channel.id, {
             content: `${choiceResult[random(choiceResult.length -1)]}`})
         }, {
