67ba815c5d76ee1cd654836d99cc59c2ed4c765a
Gaurav Gupta <gg873702@gmail.com>
2018-07-15 20:53:43 +0530

    Made the search function a tad bit better.

--- commands/helperFunctions/helperFunctions.js
+++ commands/helperFunctions/helperFunctions.js
@@ -32,15 +32,32 @@ module.exports.serviceSearch = async function (bot, message, details, searchItem
         }
         bot.createMessage(message.channel.id, {content: '', embed: embedAll}).then((msg) => {
             setTimeout(() => {
-                bot.getMessages(msg.channel.id, 10, undefined, msg.id).then((messageArray) => {
-                    messageArray.forEach(async (messageRes) => {
+                bot.getMessages(msg.channel.id, 10, undefined, msg.id).then(async (messageArray) => {
+                    /*messageArray.forEach(async (messageRes) => {
                         if (messageRes.author === message.author && parseInt(messageRes.content) <= searchItems.length) {
                             let res = await pickItem(searchItems[parseInt(messageRes.content) - 1], bot);
                             bot.createMessage(message.channel.id, {content: '', embed: res}).catch((err) => {
                                 console.log(err.stack)
+                            });
+                        }
+                    })*/
+                    let messageFound = false;
+                    for (let i = 0; i < messageArray.length; i++) {
+                        if (messageArray[i].author === message.author && parseInt(messageArray[i].content) <= searchItems.length) {
+                            messageFound = true;
+                            let res = await pickItem(searchItems[parseInt(messageArray[i].content) - 1], bot);
+                            bot.createMessage(message.channel.id, {content: '', embed: res}).catch((err) => {
+                                console.log(err.stack)
+                            });
+                        }
+                        if (messageFound) {
+                            break;
+                        } else {
+                            bot.createMessage(message.channel.id, {content: "408 Request Timed Out. (Each Search has a timeout of 7 seconds.)"}).catch((err) => {
+                                console.log(err.stack)
                             })
                         }
-                    })
+                    }
                 }).catch(err => console.log(err))
             }, 7000)
         })
