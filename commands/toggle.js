efd3e566ae93080f354c2b3b2446723ef7af4e32
EraTheMonologuer <gg873702@gmail.com>
2018-09-28 16:07:40 +0530

    Fixed a lot of the SQL Injections. Also fixed the issue where servers werent getting recognized due to IndexOf (Im a dumdum), also fixed some other shit.

--- commands/toggle.js
+++ commands/toggle.js
@@ -4,69 +4,96 @@ const fs = require('fs');
 
 module.exports.make = async (bot, con) => {
 
-    con.query("CREATE DATABASE IF NOT EXISTS 00Unit;");
-    con.query("USE 00Unit;");
-    con.query("CREATE TABLE IF NOT EXISTS commands(" +
-        "id int(4) PRIMARY KEY AUTO_INCREMENT," +
-        "name varchar(50) NOT NULL);");
-    con.query("CREATE TABLE IF NOT EXISTS servers(" +
-        "id int(10) PRIMARY KEY AUTO_INCREMENT," +
-        "code bigint NOT NULL," +
-        "name VARCHAR(1024) NOT NULL," +
-        "CONSTRAINT UNIQUE(code, name));");
-    con.query("CREATE TABLE IF NOT EXISTS whiteListedCommands(" +
-        "id int(4) PRIMARY KEY AUTO_INCREMENT," +
-        "commandID int(4) NOT NULL," +
-        "serverID int(4) NOT NULL," +
-        "CONSTRAINT FOREIGN KEY (commandID) REFERENCES commands(id)," +
-        "CONSTRAINT FOREIGN KEY (serverID) REFERENCES servers(id)," +
-        "CONSTRAINT UNIQUE (commandID, serverID)" +
-        ");");
+    con.query(`CREATE DATABASE IF NOT EXISTS 00Unit;`);
+    con.query(`USE 00Unit;`);
+    con.query(`
+      CREATE TABLE IF NOT EXISTS commands (
+        id   int(4) PRIMARY KEY AUTO_INCREMENT,
+        name varchar(50) NOT NULL
+      );`);
+    con.query(`
+      CREATE TABLE IF NOT EXISTS servers (
+        id   int(10) PRIMARY KEY AUTO_INCREMENT,
+        code varchar(50) NOT NULL UNIQUE
+      );`);
+
+    con.query(`
+      CREATE TABLE IF NOT EXISTS whiteListedCommands (
+        id        int(4) PRIMARY KEY AUTO_INCREMENT,
+        commandID int(4) NOT NULL,
+        serverID  int(4) NOT NULL,
+        CONSTRAINT FOREIGN KEY (commandID) REFERENCES commands (id),
+        CONSTRAINT FOREIGN KEY (serverID) REFERENCES servers (id),
+        CONSTRAINT UNIQUE (commandID, serverID)
+      );`);
+
+    con.query(`
+      CREATE TABLE IF NOT EXISTS userWishlists (
+        id            int PRIMARY KEY AUTO_INCREMENT,
+        userID        varchar(255) NOT NULL,
+        salePercent   int(3)          DEFAULT 75,
+        steamUsername varchar(255) NOT NULL
+      );
+    `);
 
     let files = fs.readdirSync('commands/');
     config.required_modules.split(' ').forEach(i => {
-        files.splice(files.indexOf(i), 1)
+        let moduleIndex = files.indexOf(i);
+        if (moduleIndex !== -1) {
+            files.splice(moduleIndex)
+        }
     });
-    con.query(`SELECT * FROM commands;`, (err, res, fields) => {
+    con.query(`SELECT *
+               FROM commands;`, (err, res,) => {
         res.forEach(i => {
-            files.splice(files.indexOf(i.name), 1);
+            let fileIndex = files.indexOf(i.name);
+            if (fileIndex !== -1) {
+                files.splice(fileIndex, 1);
+            }
         });
         files.forEach(i => {
             con.query(`INSERT INTO commands(name) VALUES("${i}");`)
         })
     });
 
+
+    function insertServersIntoDB(servers, con) {
+        con.query(`SELECT *
+                   FROM servers;`, (err, res) => {
+            res.forEach(i => {
+                let serverIndex = servers.indexOf(i.id);
+                if (serverIndex !== -1) {
+                    servers.splice(serverIndex, 1)
+                }
+            });
+            servers.forEach(i => {
+                con.query(`INSERT IGNORE INTO servers(code) VALUES("${i}") ;`)
+            })
+        })
+    }
+
     bot.on('ready', () => {
         let servers = [];
         bot.guilds.map(res => {
-            servers.push({id: `${res.id}`, name: `${res.name}`})
+            servers.push(res.id)
         });
-        con.query(`SELECT * FROM servers;`, (err, res, fields) => {
-            res.forEach(i => {
-                servers.splice(servers.indexOf({id: `${i.code}`, name: `${i.name}`}), 1);
-            });
-            servers.forEach(i => {
-                con.query(`INSERT INTO servers(code, name) VALUES("${i.id}", "${i.name}");`)
-            })
-        })
+        insertServersIntoDB(servers, con)
     });
+
     bot.on('guildCreate', (guild) => {
-        let servers = [{id: `${guild.id}`, name: `${guild.name}`}];
-        con.query(`SELECT * FROM servers;`, (err, res, fields) => {
-            res.forEach(i => {
-                servers.splice(servers.indexOf({id: `${i.code}`, name: `${i.name}`}), 1);
-            });
-            servers.forEach(i => {
-                con.query(`INSERT INTO servers(code, name) VALUES("${i.id}", "${i.name}");`)
-            })
-        })
+        let server = [guild.id];
+        insertServersIntoDB(server, con)
     });
-    bot.registerCommand("modules", (message, args) => {
+    bot.registerCommand("modules", (message) => {
         if (message.channel.type === 1) {
-            bot.createMessage(message.channel.id, {content: "Bot disabled in DM channels"});
+            bot.createMessage(message.channel.id, {content: "Bot disabled in DM channels"})
+                .catch(err => {
+                    console.log(err.stack)
+                });
             return
         }
-        con.query(`SELECT * FROM commands;`, (err, res, fields) => {
+        con.query(`SELECT *
+                   FROM commands;`, (err, res) => {
             let moduleList = fs.readdirSync('commands/');
             config.required_modules.split(' ').forEach(i => {
                 moduleList.splice(moduleList.indexOf(i), 1)
@@ -93,118 +120,153 @@ module.exports.make = async (bot, con) => {
             bot.createMessage(message.channel.id, {
                 content: '',
                 embed: embedAll
-            })
+            }).catch(err => console.log(err))
         })
     });
     bot.registerCommand("enable", async (message, args) => {
         if (message.channel.type === 1) {
-            bot.createMessage(message.channel.id, {content: "Bot disabled in DM channels"});
+            bot.createMessage(message.channel.id, {content: "Bot disabled in DM channels"})
+                .catch(err => console.log(err));
             return
         }
         let contentString = "";
-        if (args.length == 0 || (isNaN(parseInt(args[0]))) && args[0] !== "all" && args[0] !== "All") {
-            bot.createMessage(message.channel.id, {content: "Invalid Usage."});
+        if (args.length === 0 || (isNaN(parseInt(args[0]))) && args[0] !== "all" && args[0] !== "All") {
+            bot.createMessage(message.channel.id, {content: "Invalid Usage."})
+                .catch(err => console.log(err));
             return
         }
         let member = message.channel.guild.members.find(m => {
-            if (m.id == message.author.id) return true;
+            if (m.id === message.author.id) return true;
         });
         let roleList = member.roles.map(r => message.channel.guild.roles.get(r).name);
-        if (member.id == config.ownerID || roleList.indexOf(config.admin_role) != -1) {
-            let [serverRes, fields] = await con.query(`SELECT * FROM servers where code = ${message.channel.guild.id};`);
+        if (member.id === config.ownerID || roleList.indexOf(config.admin_role) !== -1) {
+            let [serverRes] = await con.query(`SELECT *
+                                               FROM servers
+                                               where code = ?;`, [message.channel.guild.id]);
             if (args[0] === "all" || args[0] === "All") {
-                let [commandRes, commandFields] = await con.query(`SELECT * FROM commands;`);
+                let [commandRes] = await con.query(`SELECT *
+                                                    FROM commands;`);
                 for (let i = 0; i < commandRes.length; i++) {
                     try {
-                        await con.query(`INSERT INTO whiteListedCommands(commandID, serverID) VALUES("${commandRes[i].id}", "${serverRes[0].id}");`);
+                        await con.query(`INSERT INTO whiteListedCommands (commandID, serverID)
+                                         VALUES (?, ?);`, [commandRes[i].id, serverRes[0].id]);
                         contentString += `Module ${commandRes[i].name.split('.')[0]} successfully enabled\n`
                     } catch (err) {
                         contentString += `Module ${commandRes[i].name.split('.')[0]} not enabled, ${err.code}: ${err.message}\n`
                     }
                 }
-                bot.createMessage(message.channel.id, {content: contentString});
+                bot.createMessage(message.channel.id, {content: contentString})
+                    .catch(err => console.log(err));
                 return
             }
             for (let i = 0; i < args.length; i++) {
                 if (isNaN(parseInt(args[i]))) {
                     return
                 } else {
-                    let [commandRes, commandFields] = await con.query(`SELECT * FROM commands where id = ${args[i]};`);
+                    let [commandRes] = await con.query(`SELECT *
+                                                        FROM commands
+                                                        where id = ?;`, [args[i]]);
 
                     try {
-                        await con.query(`INSERT INTO whiteListedCommands(commandID, serverID) VALUES("${commandRes[0].id}", "${serverRes[0].id}");`);
+                        await con.query(`INSERT INTO whiteListedCommands (commandID, serverID)
+                                         VALUES (?, ?);`, [commandRes[0].id, serverRes[0].id]);
                         contentString += `Module ${commandRes[0].name.split('.')[0]} successfully enabled\n`
                     } catch (err) {
                         contentString += `Module ${commandRes[0].name.split('.')[0]} not enabled, ${err.code}: ${err.message}\n`
                     }
                 }
             }
-            bot.createMessage(message.channel.id, {content: contentString});
+            bot.createMessage(message.channel.id, {content: contentString})
+                .catch(err => console.log(err));
 
         } else {
             bot.createMessage(message.channel.id, {content: `You do not have the ${config.admin_role} role.`})
+                .catch(err => console.log(err));
         }
     });
     bot.registerCommand("disable", async (message, args) => {
         if (message.channel.type === 1) {
-            bot.createMessage(message.channel.id, {content: "Bot disabled in DM channels"});
+            bot.createMessage(message.channel.id, {content: "Bot disabled in DM channels"})
+                .catch(err => console.log(err));
             return
         }
         let contentString = "";
-        if (args.length == 0 || (isNaN(parseInt(args[0]))) && args[0] !== "all" && args[0] !== "All") {
-            bot.createMessage(message.channel.id, {content: "Invalid Usage."});
+        if (args.length === 0 || (isNaN(parseInt(args[0]))) && args[0] !== "all" && args[0] !== "All") {
+            bot.createMessage(message.channel.id, {content: "Invalid Usage."})
+                .catch(err => console.log(err));
             return
         }
         let member = message.channel.guild.members.find(m => {
-            if (m.id == message.author.id) return true;
+            if (m.id === message.author.id) return true;
         });
         let roleList = member.roles.map(r => message.channel.guild.roles.get(r).name);
-        if (member.id == config.ownerID || roleList.indexOf(config.admin_role) != -1) {
-            let [serverRes, fields] = await con.query(`SELECT * FROM servers where code = ${message.channel.guild.id};`);
+        if (member.id === config.ownerID || roleList.indexOf(config.admin_role) !== -1) {
+            let [serverRes] = await con.query(`SELECT *
+                                               FROM servers
+                                               where code = ?;`, [message.channel.guild.id]);
             if (args[0] === "all" || args[0] === "All") {
-                let [commandRes, commandFields] = await con.query(`SELECT * FROM commands;`);
+                let [commandRes] = await con.query(`SELECT *
+                                                    FROM commands;`);
                 for (let i = 0; i < commandRes.length; i++) {
                     try {
-                        await con.query(`DELETE FROM whiteListedCommands WHERE commandID=${commandRes[i].id} AND serverID=${serverRes[0].id};`);
+                        await con.query(`DELETE
+                                         FROM whiteListedCommands
+                                         WHERE commandID = ?
+                                           AND serverID = ?;`, [commandRes[i].id, serverRes[0].id]);
                         contentString += `Module ${commandRes[i].name.split('.')[0]} successfully disabled\n`
                     } catch (err) {
                         contentString += `Module ${commandRes[i].name.split('.')[0]} not disabled, ${err.code}: ${err.message}\n`
                     }
                 }
-                bot.createMessage(message.channel.id, {content: contentString});
+                bot.createMessage(message.channel.id, {content: contentString})
+                    .catch(err => console.log(err));
                 return
             }
             for (let i = 0; i < args.length; i++) {
                 if (isNaN(parseInt(args[i]))) {
                     return
                 } else {
-                    let [commandRes, commandFields] = await con.query(`SELECT * FROM commands where id = ${args[i]};`);
+                    let [commandRes] = await con.query(`SELECT *
+                                                        FROM commands
+                                                        where id = ?;`, [args[i]]);
 
                     try {
-                        await con.query(`DELETE FROM whiteListedCommands WHERE commandID=${commandRes[0].id} AND serverID=${serverRes[0].id};`);
+                        await con.query(`DELETE
+                                         FROM whiteListedCommands
+                                         WHERE commandID = ?
+                                           AND serverID = ?;`, [commandRes[0].id, serverRes[0].id]);
                         contentString += `Module ${commandRes[0].name.split('.')[0]} successfully disabled\n`
                     } catch (err) {
                         contentString += `Module ${commandRes[0].name.split('.')[0]} not disabled, ${err.code}: ${err.message}\n`
                     }
                 }
             }
-            bot.createMessage(message.channel.id, {content: contentString});
+            bot.createMessage(message.channel.id, {content: contentString})
+                .catch(err => console.log(err));
 
         } else {
             bot.createMessage(message.channel.id, {content: `You do not have the ${config.admin_role} role.`})
+                .catch(err => console.log(err));
         }
     });
-    bot.registerCommand("enabledList", async (message, args) => {
+    bot.registerCommand("enabledList", async (message) => {
         if (message.channel.type === 1) {
-            bot.createMessage(message.channel.id, {content: "Bot disabled in DM channels"});
+            bot.createMessage(message.channel.id, {content: "Bot disabled in DM channels"})
+                .catch(err => console.log(err));
             return
         }
-        let [serverRes, Serverfields] = await con.query(`SELECT * FROM servers WHERE code=${message.channel.guild.id};`);
-        let [whiteListRes, fields] = await con.query(`SELECT * FROM whiteListedCommands WHERE serverID = ${serverRes[0].id};`);
+        let [serverRes] = await con.query(`SELECT *
+                                           FROM servers
+                                           WHERE code = ?;`, [message.channel.guild.id]);
+        let [whiteListRes] = await con.query(`SELECT *
+                                              FROM whiteListedCommands
+                                              WHERE serverID = ?;`, [serverRes[0].id]);
         let moduleList = [];
-        let s = [];
+
         for (let i = 0; i < whiteListRes.length; i++) {
-            let [commandRes, commandFields] = await con.query(`SELECT * FROM commands where id = ${whiteListRes[i].commandID};`);
+            let [commandRes] = await con.query(`SELECT *
+                                                FROM commands
+                                                where id = ?;`, [whiteListRes[i].commandID]);
             moduleList.push({id: `${commandRes[0].id}`, name: `${commandRes[0].name}`});
         }
         let embedAll = {
@@ -226,15 +288,22 @@ module.exports.make = async (bot, con) => {
         bot.createMessage(message.channel.id, {
             content: '',
             embed: embedAll
-        })
+        }).catch(err => console.log(err))
     })
 
 };
 
 module.exports.checkEnabled = async function (guildID, moduleName, con) {
-    let [serverRes, Serverfields] = await con.execute(`SELECT * FROM servers where code = ${guildID};`);
-    let [commandRes, Commandfields] = await con.execute(`SELECT * FROM commands where name = "${moduleName}";`);
-    let [res, fields] = await con.execute(`SELECT * FROM whiteListedCommands where commandID = ${commandRes[0].id} AND serverID = ${serverRes[0].id};`);
+    //let [serverRes] = await con.execute(`SELECT * FROM servers where code = ${guildID};`);
+    //let [commandRes] = await con.execute(`SELECT * FROM commands where name = "${moduleName}";`);
+    let [res] = await con.query(`
+      select *
+      from whiteListedCommands
+             inner join commands on commands.id = whiteListedCommands.commandID
+             inner join servers on servers.id = whiteListedCommands.serverID
+      where servers.code = ?
+        AND commands.name = ?
+    `, [guildID, moduleName]);
     if (res.length === 0) {
         return [false, `The ${moduleName.split('.')[0]} module has been disabled by the admins. To enable, do \\${config.prefix}modules `]
     } else {
