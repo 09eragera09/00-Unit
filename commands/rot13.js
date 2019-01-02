78de2b7cc704c2b3f98fe294cda04d26ec89feef
Sabarna Chakravarty <sabarnac@gmail.com>
2019-01-03 03:21:59 +0530

    Add Escape Underscore Support To Rot13
    Add support for escaping underscores in the "game name" argument for the
    rot13 command so that underscores that are part of the name of the game
    can be added without them being converted into spaces.

--- commands/rot13.js
+++ commands/rot13.js
@@ -5,11 +5,11 @@ let moduleName = path.basename(__filename);
 
 //TODO FIX THIS FUCKING THING YOU AUTISTIC FUCK
 
-module.exports.make = async (bot, conn) => {
+module.exports.make = async(bot, conn) => {
     ``;
-    bot.registerCommand("spoiler", async (message, args) => {
+    bot.registerCommand("spoiler", async(message, args) => {
         if (message.channel.type === 1) {
-            bot.createMessage(message.channel.id, {content: "Bot disabled in DM channels"})
+            bot.createMessage(message.channel.id, { content: "Bot disabled in DM channels" })
                 .catch(err => console.log(err.stack));
             return
         }
@@ -25,15 +25,18 @@ module.exports.make = async (bot, conn) => {
         if (args.length < 2) {
             return
         }
-        let game = args.shift();
-        let spoiler = args.join(' ').replace(/[a-zA-Z]/g, function (c) {
+
+        // Splits the name by escaped underscore, replaces escaped underscores
+        // with spaces in each part, and rejoins the parts with an underscore.
+        let game = args.shift().split(/\\_/).map(part => part.replace("_", " ")).join('_');
+        let spoiler = args.join(' ').replace(/[a-zA-Z]/g, function(c) {
             return String.fromCharCode((c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
         });
         let embed = {
             color: 0x91244e,
             type: 'rich',
             author: {
-                name: `Spoiler for ${game.split('_').join(' ')} by ${message.author.username}#${message.author.discriminator}`
+                name: `Spoiler for ${game} by ${message.author.username}#${message.author.discriminator}`
             },
             description: `${spoiler}`,
             footer: {
@@ -49,7 +52,7 @@ module.exports.make = async (bot, conn) => {
         })
     }, {
         description: "Parses spoilers",
-        fullDescription: "Takes 2 Arguments: Game Name in a single word, and the spoiler itself\n Eg. !spoiler GTAV Trevor is the nicest of the bunch\nParses spoilers through Rot13 for easy posting and viewing without ruining others' experience."
+        fullDescription: "Takes 2 Arguments: Game name in a single word (spaces replaced with underscore), and the spoiler itself.\n Eg. !spoiler GTA_V Trevor is the nicest of the bunch.\nParses spoilers through Rot13 for easy posting and viewing without ruining others' experience."
     });
     bot.on("messageReactionAdd", (message, emoji, userID) => {
         bot.getMessage(message.channel.id, message.id).then(msg => {
@@ -65,27 +68,27 @@ module.exports.make = async (bot, conn) => {
             }
             try {
                 if (userID !== bot.user.id && msg.author.id === bot.user.id && msg.embeds.length !== 0) {
-                bot.getDMChannel(userID).then(res => {
-                    let embed = {
-                        color: 0x91244e,
-                        type: 'rich',
-                        author: {
-                            name: `${msg.embeds[0].author.name}`
-                        },
-                        description: `${msg.embeds[0].description.replace(/[a-zA-Z]/g, function (c) {
+                    bot.getDMChannel(userID).then(res => {
+                        let embed = {
+                            color: 0x91244e,
+                            type: 'rich',
+                            author: {
+                                name: `${msg.embeds[0].author.name}`
+                            },
+                            description: `${msg.embeds[0].description.replace(/[a-zA-Z]/g, function (c) {
                             return String.fromCharCode((c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
                         })}`,
-                        footer: {
-                            text: "Spoiler support provided by 00-unit, a shitty bot written in JS by EraTheMonologuer",
-                            icon_url: bot.user.avatarURL
-                        }
-                    };
-                    bot.createMessage(res.id, {
-                        content: '',
-                        embed: embed,
-                    }).catch(err => console.log(err.stack))
-                });
-            }
+                            footer: {
+                                text: "Spoiler support provided by 00-unit, a shitty bot written in JS by EraTheMonologuer",
+                                icon_url: bot.user.avatarURL
+                            }
+                        };
+                        bot.createMessage(res.id, {
+                            content: '',
+                            embed: embed,
+                        }).catch(err => console.log(err.stack))
+                    });
+                }
             } catch (err) {
                 console.log(err);
             }
